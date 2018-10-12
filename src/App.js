b770556e1d74002e644fdd0b02d1893dfa4cc958
Settltoshi <>
2018-10-12 21:45:53 +0400

    initialize maker, fetch prices on load

--- src/App.js
+++ src/App.js
@@ -1,34 +1,47 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import getWeb3 from './utils/getWeb3';
+import React, { Component } from "react";
+import Maker from "@makerdao/dai";
+import getWeb3 from "./utils/getWeb3";
+import logo from "./logo.svg";
+import "./App.css";
+
+const maker = Maker.create("browser");
 
 class App extends Component {
   constructor(props) {
-    super(props)
+    super(props);
 
     this.state = {
       web3: null,
       account: null
-    }
+    };
   }
 
-  componentDidMount() {
+  async componentDidMount() {
     // Get network provider and web3 instance.
     // See utils/getWeb3 for more info.
+    try {
+      const web3 = await getWeb3;
+      const accs = await web3.eth.getAccounts();
+      this.setState({ account: accs[0], web3 });
+    } catch (e) {
+      console.log(e, "Error finding web3.");
+    }
 
-    getWeb3.then(results => {
+    try {
+      await maker.authenticate();
+      const priceService = maker.service("price");
+      const [ethPrice, mkrPrice] = await Promise.all([
+        priceService.getEthPrice(),
+        priceService.getMkrPrice()
+      ]);
+
       this.setState({
-        web3: results.web3
-      })  
-      results.web3.eth.getAccounts().then((accs) => {
-        this.setState({
-          account: accs[0]
-        })
-      })
-    }).catch((e) => {
-      console.log(e,'Error finding web3.')
-    })
+        ethPrice: ethPrice.toString(),
+        mkrPrice: mkrPrice.toString()
+      });
+    } catch (e) {
+      console.log(e, "Failed to initialize maker");
+    }
   }
 
   render() {
@@ -36,8 +49,12 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">{this.state.account}</h1>
+          <h1 className="App-title">Settle Maker</h1>
         </header>
+        {!this.state.account && 'Loading...'}
+        <div>Logged in as {this.state.account}</div>
+        <div>{this.state.ethPrice}</div>
+        <div>{this.state.mkrPrice}</div>
       </div>
     );
   }
--- src/utils/getWeb3.js
+++ src/utils/getWeb3.js
@@ -3,7 +3,6 @@ import Web3 from 'web3'
 let getWeb3 = new Promise(function(resolve, reject) {
   // Wait for loading completion to avoid race conditions with web3 injection timing.
   window.addEventListener('load', function() {
-    var results
     var web3 = window.web3
 
     // Checking if Web3 has been injected by the browser (Mist/MetaMask)
@@ -11,13 +10,9 @@ let getWeb3 = new Promise(function(resolve, reject) {
       // Use Mist/MetaMask's provider.
       web3 = new Web3(web3.currentProvider)
 
-      results = {
-        web3: web3
-      }
-
       console.log('Injected web3 detected.');
 
-      resolve(results)
+      resolve(web3)
     } else {
       // Fallback to localhost if no web3 injection. We've configured this to
       // use the development console's port by default.
@@ -25,13 +20,9 @@ let getWeb3 = new Promise(function(resolve, reject) {
 
       web3 = new Web3(provider)
 
-      results = {
-        web3: web3
-      }
-
       console.log('No web3 instance injected, using Local web3.');
 
-      resolve(results)
+      resolve(web3)
     }
   })
 })
