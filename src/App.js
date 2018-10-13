7811fd63a027c6c3e4a22b1fda930ef29ae00dbb
Settltoshi <44066453+settltoshi@users.noreply.github.com>
2018-10-14 01:10:07 +0400

    loading state, account change

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import Maker from "@makerdao/dai";
-import { Container } from 'semantic-ui-react'
-import CDPList from './CDPList'
+import { Container, Loader } from "semantic-ui-react";
+import CDPList from "./CDPList";
 import getWeb3 from "./utils/getWeb3";
 
 const maker = Maker.create("browser");
@@ -17,11 +17,22 @@ class App extends Component {
   }
 
   async componentDidMount() {
+    const web3 = await getWeb3;
+    web3.currentProvider.publicConfigStore.on("update", this.initializeAccount);
+    this.initializeAccount();
+  }
+
+  initializeAccount = async () => {
     // Get network provider and web3 instance.
     // See utils/getWeb3 for more info.
     try {
       const web3 = await getWeb3;
       const accs = await web3.eth.getAccounts();
+      if (accs.length === 0) {
+        this.setState({ account: "" });
+        return;
+      }
+      if (accs[0] === this.state.account) return;
       this.setState({ account: accs[0], web3 });
     } catch (e) {
       console.log(e, "Error finding web3.");
@@ -42,7 +53,7 @@ class App extends Component {
     } catch (e) {
       console.log(e, "Failed to initialize maker");
     }
-  }
+  };
 
   render() {
     return (
@@ -50,12 +61,25 @@ class App extends Component {
         <header className="App-header">
           <h1 className="App-title">Settle Maker</h1>
         </header>
-        {!this.state.account && 'Loading...'}
+        {!!this.state.account ? this.renderContent() : this.renderLoading()}
+      </Container>
+    );
+  }
+
+  renderLoading() {
+    return <Loader active>Metamask Locked?</Loader>;
+  }
+
+  renderContent() {
+    return (
+      <>
         <div>Logged in as {this.state.account}</div>
         <div>{this.state.ethPrice}</div>
         <div>{this.state.mkrPrice}</div>
-        {this.state.account && <CDPList address={this.state.account} />}
-      </Container>
+        {this.state.account && (
+          <CDPList key={this.state.account} address={this.state.account} />
+        )}
+      </>
     );
   }
 }
