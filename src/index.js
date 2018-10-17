ec543baa87a4422acd19d2e4aa878690f04b04bd
Settltoshi <44066453+settltoshi@users.noreply.github.com>
2018-10-18 01:37:42 +0400

    darker theme

--- src/index.js
+++ src/index.js
@@ -4,7 +4,8 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import store from './store'
 
-import 'semantic-ui-css/semantic.min.css';
+//import 'semantic-ui-css/semantic.min.css';
+import './semantic.cyborg.css'
 
 ReactDOM.render(
 <div>
