4a325dd017c40b90953b5ef82140cd57b4c9cb44
Settltoshi <44066453+settltoshi@users.noreply.github.com>
2018-10-21 14:41:06 +0400

    make lock modal more consistent with free

--- src/cdp/Modals/Lock.js
+++ src/cdp/Modals/Lock.js
@@ -1,18 +1,20 @@
 import React, { Component } from "react";
-import { Button, Modal, Header, Form } from "semantic-ui-react";
+import { Button, Modal, Header, Form, Input } from "semantic-ui-react";
 
 export default class Lock extends Component {
   state = {
     amountETH: this.props.store.ethBalance.get().toNumber(),
-    amountPETH: (
-      this.props.store.ethBalance.get().toNumber() /
-      this.props.store.wethToPeth.get()
-    ).toFixed(6),
     color: "gray",
     valid: false
   };
 
   render() {
+    const wethToPeth = this.props.store.wethToPeth.get();
+    const amountPETH = (this.state.amountETH / wethToPeth).toFixed(4);
+
+    const pethLocked = this.props.cdp.pethLocked;
+    const ethLocked = pethLocked.toNumber() * wethToPeth;
+
     return (
       <Modal open closeIcon onClose={this.props.onRequestClose}>
         <Header>Lock ETH</Header>
@@ -24,30 +26,23 @@ export default class Lock extends Component {
             paddingBottom: "0"
           }}
         >
-          PETH Locked: {this.props.cdp.pethLocked.toString(4)}
-        </Header>
-
-        <Header
-          as="h5"
-          style={{
-            color: this.state.color,
-            display: "inline",
-            paddingBottom: "0"
-          }}
-        >
-          ETH -> PETH: {this.state.amountPETH}
+          Locked: {pethLocked.toString(4)} ({ethLocked.toFixed(4)} ETH)
         </Header>
         <Modal.Content>
           <Form>
-            <Form.Input
-              name={"amountETH"}
-              label={"ETH to lock"}
-              placeholder="ETH to lock"
-              type="number"
-              step="0.0001"
-              value={this.state.amountETH}
-              onChange={this.handleChange}
-            />
+            <Form.Field>
+              <label>ETH to lock</label>
+              <Input
+                name={"amountETH"}
+                label={{ basic: true, content: `${amountPETH} PETH` }}
+                labelPosition={"right"}
+                placeholder="ETH to lock"
+                type="number"
+                step="0.0001"
+                value={this.state.amountETH}
+                onChange={this.handleChange}
+              />
+            </Form.Field>
           </Form>
         </Modal.Content>
         <Modal.Actions>
@@ -72,11 +67,6 @@ export default class Lock extends Component {
   };
 
   handleChange = (e, { name, value }) => {
-    this.setState({ [name]: value }, () => {
-      const amountPETH = (
-        this.state.amountETH / this.props.store.wethToPeth.get()
-      ).toFixed(6);
-      this.setState({ amountPETH: amountPETH });
-    });
+    this.setState({ [name]: value });
   };
 }
