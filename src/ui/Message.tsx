2e7a383cce037432ef426cb401bc5309882748a8
Settltoshi <44066453+settltoshi@users.noreply.github.com>
2018-11-18 03:17:17 +0400

    message style

--- src/ui/Message.tsx
+++ src/ui/Message.tsx
@@ -4,24 +4,10 @@ import styled from "styled-components";
 //import { ErrorOutline as Alert } from "styled-icons/material";
 import { AlertTriangle as Alert } from "styled-icons/feather/";
 
-const StyledMessage = styled.div`
+export const Message = styled.div`
   display: flex;
   color: #ededed;
   font-size: 16px;
   align-items: center;
+  color: #ff5252;
 `;
-
-interface Props {
-  children?: React.ReactNode;
-}
-
-export default function Message(props: Props) {
-  return (
-    <StyledMessage>
-      <div style={{ padding: "0 10px" }}>
-        <Alert size={40} color={"#F44336"} />
-      </div>
-      <div>{props.children}</div>
-    </StyledMessage>
-  );
-}
--- src/ui/index.tsx
+++ src/ui/index.tsx
@@ -10,7 +10,7 @@ export { default as Dimmer } from "./Dimmer";
 export { default as Input } from "./Input";
 export { Header, Header2 } from "./Header";
 export { default as Loader } from "./Loader";
-export { default as Message } from "./Message";
+export { Message } from "./Message";
 
 export function Container(props: any) {
   return <div />;
